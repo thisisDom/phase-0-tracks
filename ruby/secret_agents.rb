9ef4e8e94abde38302c95940700852494141c481
Dominique Crowther <Dominique.Crowther@gmail.com>
2017-03-30 19:48:13 -0700

    added decrypt method to secret_agent.rb

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,3 +1,4 @@
+
 #Define two methods "encrypt" and "decrypt"
 #Define encrypt method with one string parameter, the word we want to encrypt
 def encrypt(word)
@@ -7,6 +8,23 @@ def encrypt(word)
     #If there is a space, move on to next letter
     if(word[index] != " ")
         word[index] = word[index].next
+    end
+    index += 1
+  end
+  return word
+end
+#Define decrypt method with one string parameter, the word we want to encrypt
+def decrypt(word)
+  #Assign alphabet to varible
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  index = 0
+  while index < word.length
+    #Add conditional statement to check if current position in word isn't a space.
+    #If there is a space, move on to next letter
+    if(word[index] != " ")
+      #Find position in the alphabet of the current letter and subtract -1 to it to get index of letter before it
+      new_index = alphabet.index(word[index])
+      word[index] = alphabet[(new_index - 1 )]
     end
     index += 1
   end
