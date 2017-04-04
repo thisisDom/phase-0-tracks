51d05af12fbafcb7f624d24e0d6693091c4c6c8e
Dominique Crowther <Dominique.Crowther@gmail.com>
2017-04-03 18:26:29 -0700

    updated method_detective.rb
    exit

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,38 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+"iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+"zom".insert(2,"o")
 # => “zoom”
 
-# "enhance".<???>
+"enhance".center(17)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+"the usual".insert(-1," suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+" suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+"The case of the disappearing last letter".chomp("r")
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+"The mystery of the missing first letter".reverse.chomp("T").reverse
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".split.join(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+"z".codepoints
 # => 122 
-# (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+# (What is the significance of the number 122 in relation to the character z?)
+  #It's the integer value for the letter z in ASCII(&#122;) and UTF-8(122)
+  
+ "How many times does the letter 'a' appear in this string?".count("a")
 # => 4
