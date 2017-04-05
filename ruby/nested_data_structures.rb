fbbf70ae38c0d04ea06ca82350a93d480d302c97
Dominique Crowther <Dominique.Crowther@gmail.com>
2017-04-04 19:07:56 -0700

    Added nested data stucture for bay area sports teams

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,111 @@
+#Created data structure for Bay Area sports teams
+
+bayareasports = {
+    basketball: {
+        "Golden State Warrior" : {
+          team_color: [
+              "Blue",
+              "Yellow",
+              "White"
+          ]
+          nicknames: []
+          home_arena: "Oracle Arena"
+          no_of_championships: 4
+          champships: [
+              1947,
+              1956,
+              1975,
+              2015
+          ]
+        }
+    }
+    baseball: {
+        "San Francisco Giants" : {
+            team_colors: [
+                "Orange",
+                "Black",
+                "Gold"
+                "Cream"
+            ]
+            nicknames: [
+                "The Orange and Black",
+                "Los Gigantes",
+                "The G-Men",
+                "The Boys from the Bay"
+            ]
+            ballpark: "AT&T Park"
+            no_of_world_series: 8
+            world_series_champs: [
+                1905,
+                1921,
+                1922,
+                1933,
+                1954,
+                2010,
+                2012,
+                2014
+            ]
+        }
+        "Oakland Athletics" : {
+            team_colors: [
+                "Green",
+                "Gold",
+                "White"
+            ]
+            nicknames: [
+                "The A's",
+                "The Swingin' A's",
+                "The Green and Gold"
+            ]
+            ballpark: "Oakland Alameda County Coliseum"
+            no_of_world_series: 9
+            world_series_champs: [
+                1910,
+                1911,
+                1913,
+                1929,
+                1930,
+                1972,
+                1973,
+                1974,
+                1989
+            ]
+        }
+    }
+    football: {
+        "San Franciso 49ers" : {
+            team_colors: [
+                "Red",
+                "Gold",
+                "Black",
+                "White",
+            ]
+            nicknames: ["Niners"]
+            home_field: "Levi's Stadium"
+            no_of_super_bowls: 5
+            super_bowl_champs: [
+                1981,
+                1984,
+                1988,
+                1989,
+                1994
+            ]
+
+        }
+    }
+    hockey: {
+        "San Jose Sharks" : {
+            team_color: [
+                "Teal",
+                "Orange",
+                "Black",
+                "White",
+            ]
+            nicknames: []
+            home_arena: "Sap Center"
+            no_of_stanley_cups: 0
+            stanley_cup_champs: []
+
+        } 
+    }
+}
