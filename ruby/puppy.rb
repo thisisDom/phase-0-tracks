94ea9773847ab98819e8d6818cacd3b592302cdf
Dominique Crowther <Dominique.Crowther@gmail.com>
2017-04-10 14:53:02 -0700

    added class design for a puppy class for 6.1:Release 0

---
+++ ruby/puppy.rb
@@ -0,0 +1,13 @@
+# Class Puppy
+
+  # Characteristics(Attributes)
+    # Age
+    # Breed
+    # Fur Color
+    # Fur Length
+  # Behavior(Methods)
+    # Bark
+    # Eat
+    # Fetch
+    # Poop
+    # Sleep
