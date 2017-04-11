1ea94b899db62fb31b659993584e4aec6ab8925b
Dominique Crowther <Dominique.Crowther@gmail.com>
2017-04-11 14:54:28 -0700

    update shout.rb added calls to methods 'yells_angrily' and ' yells_happily

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,6 +1,11 @@
 # Module Shout
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def self.yells_angrily(words)
+    return words + "!!!" + " :("
+  end
+  def self.yells_happily(words)
+    return words + "!!!" + " :)"
   end
 end
+puts Shout.yells_angrily("I'm so angry right now")
+puts Shout.yells_happily("I'm so happy right now")  
