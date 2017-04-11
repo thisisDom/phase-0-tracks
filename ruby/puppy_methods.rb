341ae2b935efeaef96d0ac2933780dbce42a0851
Dominique Crowther <Dominique.Crowther@gmail.com>
2017-04-11 11:39:41 -0700

    Updated to complete release 2 for 6.3 pairing

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -32,3 +32,36 @@ bumper.speak(4)
 bumper.roll_over
 puts bumper.dog_years(2)
 bumper.play_dead
+
+class Runner
+  def initialize
+    "Initializing new Runner instance"
+  end
+  
+  def jogs
+    puts "*jogs*"
+  end
+  
+  def sprints
+    puts "*sprints*"
+  end
+  
+  def hundred_meter_time(time)
+    puts time * 10
+  end
+  
+end
+
+runners = []
+usain_bolt = Runner.new
+50.times do 
+  runners << Runner.new
+end
+usain_bolt.jogs
+usain_bolt.sprints
+usain_bolt.hundred_meter_time(9.59)
+runners.each do |runner|
+  usain_bolt.jogs
+  usain_bolt.sprints
+  usain_bolt.hundred_meter_time(Random.rand(8.59..10.00))
+end
