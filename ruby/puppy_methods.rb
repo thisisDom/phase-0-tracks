f9c447f02fbe3ead77abe9013c9fcc7e06e85550
Dominique Crowther <Dominique.Crowther@gmail.com>
2017-04-11 11:22:58 -0700

    updated to complete release 1 for 6.2 pairing

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,10 +1,24 @@
 class Puppy
+  def initialize
+    puts "Initializing new puppy instance"
+  end
+  
+  def dog_years(human_years)
+    return human_years * 7
+  end   
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
-
+  
+  def play_dead
+    puts "*plays dead*"
+  end
+  def roll_over
+    puts "*rolls over*"
+  end
+  
   def speak(integer)
     integer.times do
       puts "Woof"
@@ -15,3 +29,6 @@ end
 bumper = Puppy.new
 bumper.fetch("ball")
 bumper.speak(4)
+bumper.roll_over
+puts bumper.dog_years(2)
+bumper.play_dead
