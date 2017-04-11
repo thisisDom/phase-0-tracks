0a49fedab6b5d095aefa0d08721fe6223bfc4a7b
Dominique Crowther <Dominique.Crowther@gmail.com>
2017-04-11 15:16:43 -0700

    creating module Shout, class Teacher, and class Student

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,11 +1,44 @@
 # Module Shout
+#module Shout
+#  def self.yells_angrily(words)
+#    return words + "!!!" + " :("
+#  end
+#  def self.yells_happily(words)
+#    return words + "!!!" + " :)"
+#  end
+#end
+#puts Shout.yells_angrily("I'm so angry right now")
+#puts Shout.yells_happily("I'm so happy right now")
+
 module Shout
-  def self.yells_angrily(words)
-    return words + "!!!" + " :("
+  def raise_hand(words)
   end
-  def self.yells_happily(words)
-    return words + "!!!" + " :)"
+  def yawns
+    if(self.class == 'Teacher')
+      puts "Pardon me, class."
+    end
+    else
+      puts "#{self.name}, please cover your mouth"
+    end
+  end
+  def yells_angrily(words)
   end
 end
-puts Shout.yells_angrily("I'm so angry right now")
-puts Shout.yells_happily("I'm so happy right now")  
+
+module Teacher
+  include Shout
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+module Student
+  include Shout
+
+  def initialize(name)
+    @name = name
+  end
+end
+teacher = Teacher.new("Mr.Kim")
+student = Student.new("Dominique")
