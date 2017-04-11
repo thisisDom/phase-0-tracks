bef250e06d118ca70d6d64eec49e4561023b026b
Dominique Crowther <Dominique.Crowther@gmail.com>
2017-04-11 15:29:04 -0700

    updated shout.rb to complete 6.4 challenge

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -11,34 +11,52 @@
 #puts Shout.yells_happily("I'm so happy right now")
 
 module Shout
-  def raise_hand(words)
+  def raises_hand
+    puts "*#{@name} raises hand*"
   end
+  
   def yawns
-    if(self.class == 'Teacher')
-      puts "Pardon me, class."
-    end
-    else
-      puts "#{self.name}, please cover your mouth"
-    end
+     puts "#{@name}, please cover your mouth"
+  end
+  
+  def yells_angrily(words)
+    puts words + "!!!"
+  end
+end
+
+class Teacher
+  include Shout
+  def initialize(name)
+    @name = name
+  end
+  
+  def raises_hand
+     puts "Why are you raising your hand, #{@name}. You're the teacher."
+  end
+  
+  def yawns
+    puts "Sorry class, I am very tired."
+  end
+  
+end
+
+class Student
+  include Shout
+  def initialize(name)
+    @name = name
   end
   def yells_angrily(words)
-  end
-end
-
-module Teacher
-  include Shout
-
-  def initialize(name)
-    @name = name
-  end
-end
-
-module Student
-  include Shout
-
-  def initialize(name)
-    @name = name
+    puts "#{@name}, please lower your voice."
   end
 end
 teacher = Teacher.new("Mr.Kim")
 student = Student.new("Dominique")
+
+teacher.raises_hand
+student.raises_hand
+
+teacher.yawns
+student.yawns
+
+teacher.yells_angrily("Keep it quiet")
+student.yells_angrily("")
