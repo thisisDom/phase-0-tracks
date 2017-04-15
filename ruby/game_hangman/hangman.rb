e832e9203a0ed5236a37b754d38b334085a55f13
Dominique Crowther <Dominique.Crowther@gmail.com>
2017-04-15 00:29:08 -0700

    updated hangman.rb and rspec hangman_spec

--- ruby/game_hangman/hangman.rb
+++ ruby/game_hangman/hangman.rb
@@ -31,22 +31,26 @@ class Hangman
       "window",
       "wolf",
     ]
-    @display_puzzle_word = []
+    @display_word = []
+    @game_status = nil
     @game_winner = nil
-    @guessed_letter = []
+    @guessed_letters = []
 
     @guess_word = @game_dictionary.sample
     @guess_limit = @guess_word.length
     @guess_word_letters = @guess_word.split("")
 
     @guess_word_letters.each do
-      @display_puzzle_word << "?"
+      @display_word << "?"
     end
   end
   # Create a method to see if games over
+  def display_word
+    return @display_word.join
+  end
   def game_over
     letters_left = 0
-    @display_puzzle_word.each do |letter|
+    @display_word.each do |letter|
       if letter == "?" 
         letters_left += 1
       end
@@ -60,12 +64,13 @@ class Hangman
         @game_winner = false
       end
     end
+    return false
   end
   # Create a method for guessing a letter
     # Gets user input
   def guess_letter(letter)
     # Make sure letter hasn't already been guessed
-    if @guessed_letter.index(letter)
+    if @guessed_letters.index(letter)
       return "Letter already guessed."
     end
     # Compare letter to word
@@ -74,50 +79,51 @@ class Hangman
     @guess_word_letters.each do |guess_word_letter|
       if  guess_word_letter == letter
         #replace question marks in display word with correct letter
-        @display_puzzle_word[letter_index] = letter
+        @display_word[letter_index] = letter
         letter_count += 1
       end
       letter_index += 1
     end
     # Store letter as a guessed letter
-    @guessed_letter << letter
+    @guessed_letters << letter
     if letter_count == 0
       @guess_limit -= 1
     end
+    return letter
   end
-  # Create a method that returns the state of the word
-  def word_status
-    puts @display_puzzle_word.join
+  def guesses_left
     guess = "guesses"
     if @guess_limit == 1
       guess = "guess"
     end
-    puts "#{@guess_limit} #{guess} left."
+    return "#{@guess_limit} #{guess} left."
   end
+  # Create a method that returns the state of the word
 end  
-# game = Hangman.new
-# puts "It's time to play a game"
-# puts "Try to see if you can guess the letter I'm thinking of"
-# game.word_status
-# until game.game_status == "done"
-#   puts "Please guess a letter."
-#   input = gets.chomp
-#   until input.length == 1
-#     puts "Please, guess a letter."
-#     input = gets.chomp
-#   end
-#   game.guess_letter(input) 
-#   game_status = game.game_over
-#   if game_status != "done"
-#     game.word_status
-#   end
-# end
-# if game.game_winner
-#   puts "Congratulation, you win"
-#   puts "Guess you outsmarted me (╯°□°）╯︵ ┻━┻"
-# else
-#   puts "Sorry, you lose"
-#   puts "The word was #{game.guess_word}"
-#   puts "Guess you aren't that smart (⌐■_■)"
-# end
-# exit
+game = Hangman.new
+puts "It's time to play a game"
+puts "Try to see if you can guess the letter I'm thinking of"
+game.display_word
+until game.game_status == "done"
+  puts "Please guess a letter."
+  input = gets.chomp
+  until input.length == 1
+    puts "Please, guess a letter."
+    input = gets.chomp
+  end
+  game.guess_letter(input) 
+  game_status = game.game_over
+  if game_status != "done"
+    puts game.display_word
+    puts game.guesses_left
+  end
+end
+if game.game_winner
+  puts "Congratulation, you win"
+  puts "Guess you outsmarted me (╯°□°）╯︵ ┻━┻"
+else
+  puts "Sorry, you lose"
+  puts "The word was #{game.guess_word}"
+  puts "Guess you aren't that smart (⌐■_■)"
+end
+exit
--- ruby/game_hangman/hangman_spec.rb
+++ ruby/game_hangman/hangman_spec.rb
@@ -7,4 +7,15 @@ describe Hangman do
     expect(game.guess_letter("l")).to eq "l"
   end
 
+  it "gets game word and displays it with question marks for unguessed letters" do
+    expect(game.display_word).to include "?"
+  end
+
+  it "gets current status of the game" do
+    expect(game.game_over).to be false
+  end
+
+  it "gets the number of guesses left in the game" do
+    expect(game.guesses_left).to include "guesses left"
+  end
 end
