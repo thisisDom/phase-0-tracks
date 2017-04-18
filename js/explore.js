02b31da674475be996a546e9eae80b527e416ac0
Dominique Crowther <Dominique.Crowther@gmail.com>
2017-04-17 19:16:33 -0700

    Pseudocoded a function that reverses a string

---
+++ js/explore.js
@@ -0,0 +1,11 @@
+// Pseudo Code for a reverse function
+
+// Create a function that takes the word as a parameter
+// find the string's length and store it in a variable
+// create an blank array that is the same length as the string
+// create a loop that iterates through each letter of the original string
+// create an equation that finds the new position of the letter in the blank array
+  // should be the (strings length-1) - current index
+// store value in letter in new array using new index
+// join the new word array
+// return the new array
