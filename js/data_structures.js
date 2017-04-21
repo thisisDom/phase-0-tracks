73f75b58de37947ef1746c50fc93b85c3e311824
Dominique Crowther <Dominique.Crowther@gmail.com>
2017-04-20 19:00:04 -0700

    adds constructor function Car and creates 2 cars

--- js/data_structures.js
+++ js/data_structures.js
@@ -17,3 +17,22 @@ if(horse_colors.length == horse_names.length){
   }
   console.log(horses);
 }
+
+function Car(manufacturer, year, color, model){
+
+  this.manufacturer = manufacturer;
+  this.year = year;
+  this.color = color;
+  this.model = model;
+
+  this.drive = function(){ console.log("It ignites!!"); };
+
+}
+
+console.log("Lets build a car");
+var car1 = new Car("Honda", "2017", "Silver", "Accord");
+console.log(car1);
+car1.drive();
+var car2 = new Car("Toyota", "2015", "Green", "Corolla");
+console.log(car2);
+car2.drive();
