a3418bc9a5d117022d6983e899e104b28184530b
Dominique Crowther <Dominique.Crowther@gmail.com>
2017-04-20 18:14:02 -0700

    creates arrays for horse names and horse colors

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var horse_colors = ["blue","red","green","yellow"];
+var horse_names = ["Black Beauty", "Seabiscuit", "Secretariat", "Curlin"];
+
+horse_colors.push("teal");
+horse_names.push("Ruffian");
+
+console.log(horse_colors);
+console.log(horse_names);
