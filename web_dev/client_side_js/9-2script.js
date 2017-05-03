b2196411f599c8200b11bd089fff78d0d4912c24
Dominique Crowther <Dominique.Crowther@gmail.com>
2017-05-02 20:08:12 -0700

    completes release 1&2 for pairing 9.2

--- web_dev/client_side_js/9-2script.js
+++ web_dev/client_side_js/9-2script.js
@@ -5,37 +5,31 @@ var chameleon = document.getElementById("lizard-photo");
 chameleon.style.visibility = "visible";
 
 var blend = document.getElementById("blend");
-
 var reveal = document.getElementById("reveal");
 
 function blendIt(){
   // select the chameleon photo and store it as a variable
   console.log("blending...");
-  // check visibility of chameleon
-  if(chameleon.style.visibility == "visible"){
-   // change style to not visibility
-    chameleon.style.visibility = "hidden";
-  }
-    // show reveal button
+ // change style to not visibility
+  chameleon.style.visibility = "hidden";
+  // hide blend button
   blend.style.display = "none";
-    // reveal button
-  reveal.style.display = "none";
+  // show reveal button
+  reveal.style.display = "inline";
 }
 // a function that reveals the chameleon
 
 function revealIt(){
   // select the chameleon photo and store it as a variable
   console.log("revealing...");
-  // check visibility of chameleon
-  if(chameleon.style.visibility == "hidden"){
-    // change style to visible if chameleon in not visible
-    chameleon.style.visibility = "visible";
-  }
-    // show blend button
-  blend.style.display = "none";
-    // hide reveal button
+  // change style to visibility
+  chameleon.style.visibility = "visible";
+  // hide reveal button
   reveal.style.display = "none";
+  // show blend button
+  blend.style.display = "inline";
 }
 
 blend.addEventListener("click", blendIt);
 reveal.addEventListener("click", revealIt);
+revealIt();
