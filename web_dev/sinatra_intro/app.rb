58cedebeb18309b3bb964082d2945c48f7f0ec83
Dominique Crowther <Dominique.Crowther@gmail.com>
2017-05-03 18:24:07 -0700

    adds search functionality

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -41,10 +41,10 @@ end
 # write a GET route that retrieves
 # a particular student
 
-get '/students/:id' do
-  student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
-  student.to_s
-end
+# get '/students/:id' do
+#   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
+#   student.to_s
+# end
 
 # A /contact route that displays an address (you can make up the address).
 
@@ -54,6 +54,7 @@ get '/contact' do
   response << "<h3>San Jose, Ca 95445</h3>"
   response
 end
+# A /great_job route that can take a person's name as a query parameter (not a route parameter) and say "Good job, [person's name]!". If the query parameter is not present, the route simply says "Good job!"
 
 get '/greatjob' do
   if params[:name]
@@ -63,7 +64,26 @@ get '/greatjob' do
   end
 end
 
+# A route that uses route parameters to add two numbers and respond with the result. The data types are tricky here -- when will the data need to be (or arrive as) a string?
 get '/add/:num1+:num2' do
   response = params[:num1].to_i + params[:num2].to_i
   "#{response}"
+end
+
+get '/students/:name' do
+  name = params[:name]
+  query = "SELECT * FROM students WHERE lower(name) LIKE \"#{name}%\" OR lower(name) LIKE \"%#{name}\" ORDER BY name"
+  students = db.execute(query)
+  response = ""
+  if students.length > 0
+    students.each do |student|
+      response << "ID: #{student['id']}<br>"
+      response << "Name: #{student['name']}<br>"
+      response << "Age: #{student['age']}<br>"
+      response << "Campus: #{student['campus']}<br><br>"
+    end
+  else 
+    response = "No Results Found"
+  end
+  response
 end
