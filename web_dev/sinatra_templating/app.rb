c0b0d2d61390632b02329a1b483740a39a062dfa
Dominique Crowther <Dominique.Crowther@gmail.com>
2017-05-07 15:13:35 -0700

    adds route to get all students at a given location

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -1,5 +1,6 @@
 # require gems
 require 'sinatra'
+require "sinatra/reloader" if development?
 require 'sqlite3'
 
 set :public_folder, File.dirname(__FILE__) + '/static'
@@ -24,4 +25,12 @@ post '/students' do
   redirect '/'
 end
 
+get '/students/:campus' do
+  query = <<-SQL
+    SELECT * FROM students WHERE LOWER(campus)=LOWER("#{params[:campus]}")
+  SQL
+  @students = db.execute(query)
+  @campus = params[:campus]
+  erb :students
+end
 # add static resources
